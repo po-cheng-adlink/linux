f2bbfbe7eb1ab2d20457d8efc4473aca481fddbe
po.cheng <po.cheng@adlinktech.com>
2023-11-24 10:47:54 +0800

    sp2-imx8mp: patch: panel-simple: parse additional delays to fine tune panel timing
    Signed-off-by: po.cheng <po.cheng@adlinktech.com>

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -568,7 +568,7 @@ static int panel_simple_parse_dt_settings (struct device *dev, struct panel_simp
 {
 	struct device_node *np = dev->of_node;
 	struct panel_desc *pd = (struct panel_desc *)desc;
-	int ret = 0;
+	int ret = 0; /* return -1 to warn override */
 
 	/* if other panel node attributes exists, parse them from device tree, and force override */
 	if (of_property_read_bool(np, "panel-width-mm")) {
@@ -577,6 +577,23 @@ static int panel_simple_parse_dt_settings (struct device *dev, struct panel_simp
 	}
 	if (of_property_read_bool(np, "panel-height-mm")) {
 		of_property_read_u32(np, "panel-height-mm", &pd->size.height);
+		ret = -1;
+	}
+	/* optional delays */
+	if (of_property_read_bool(np, "delay-prepare")) {
+		of_property_read_u32(np, "delay-prepare", &pd->delay.prepare);
+		ret = -1;
+	}
+	if (of_property_read_bool(np, "delay-enable")) {
+		of_property_read_u32(np, "delay-enable", &pd->delay.enable);
+		ret = -1;
+	}
+	if (of_property_read_bool(np, "delay-disable")) {
+		of_property_read_u32(np, "delay-disable", &pd->delay.disable);
+		ret = -1;
+	}
+	if (of_property_read_bool(np, "delay-unprepare")) {
+		of_property_read_u32(np, "delay-disable", &pd->delay.unprepare);
 		ret = -1;
 	}
 	if (of_property_read_bool(np, "refresh-rate")) {
